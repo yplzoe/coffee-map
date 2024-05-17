2c65bc67e7fc7cf0baed089a1dc9bd6da13787e8
yplzoe <103309763+yplzoe@users.noreply.github.com>
2024-05-17 15:38:51 +0800

    refactor: add global variable to config.py for easier maintenance

---
+++ config.py
@@ -0,0 +1,15 @@
+
+tag_translations = {
+    "brew": '手沖',
+    "coffee": '咖啡',
+    "beans": '咖啡豆',
+    "internet": '網路',
+    "socket": '插座',
+    "seat": '座位',
+    "desert": '甜點',
+    "pet": '寵物',
+    "work": '適合工作',
+    "comfort": '氛圍',
+    "quiet": '安靜',
+    "time": '不限時'
+}
